956c8fcc06ca54942364ee1f793f3ee4e81b090e
Payton McIntosh <pmcintosh@df12.net>
2025-10-29 17:47:31 +0000

    Bottom-align map viewport content

--- src/app/components/map-viewport.tsx
+++ src/app/components/map-viewport.tsx
@@ -43,7 +43,7 @@ export function MapViewport({
   map,
 }: MapViewportProps): JSX.Element {
   const containerClasses = joinClassNames(
-    "relative flex flex-1 min-h-0 flex-col overflow-hidden",
+    "relative flex flex-1 min-h-0 flex-col justify-end overflow-hidden",
     className,
   );
 
